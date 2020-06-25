c6dd6e247e6a08516eba7370b5edac2d2e0365fd
MohanRaj-M <mohanraj@iexemplar.com>
2020-06-25 21:17:41 +0530

    asset folder

---
+++ assets/js/zoho.js
@@ -0,0 +1,5 @@
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || 
+{widgetcode:"cdd293f73456f019a25836f2c48054b2c738f03e5fa99a0946898935eac27fdc", values:{},ready:function(){}};
+var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+d.write("<div id='zsiqwidget'></div>");
---
+++ assets/static-data/staticText.ts
@@ -0,0 +1,24 @@
+export const staticText = {
+  tooltips: {
+    account_name: `
+      Give a meaningful and unique name so you can identify what information you have in this account.<br>
+      For example, it can be MyGmail or, MyPayPal, PrimaryHome-Mortgage etc.When creating an Account, you will notice, there are several fields that let you input more information about this account.
+      Don’t worry if you can’t fill all the fields. 
+      You can use the notes section to store additional details about an account.`,
+    password_hint: `
+      DO NOT STORE PASSWORD.<br>
+      A Password hint is a cue that enables you to remember your actual password. You can have a decoder on a separate location and share it separately.<br>
+      For example, if your password is 1969Ray78759, a combination of : <br>
+      - Your Year of birth – 1969 <br>
+      - Your first name – Ray <br>
+      - Your zip code – 78759 <br>
+      You can create a Password Hint for this password, such as: YrBornNameZip<br>
+      We don’t recommend saving your actual password, which is why we give you the safer route of setting password hints.`,
+    account_group: `
+      The Account Group allows you to organize your Accounts and group them by relevance. Think of the account group as a drawer in the file cabinet.<br>
+      For example, you can bundle all your home-related accounts under the ‘Home’ Account Group, and all your finance related accounts under the ‘Financial’ Account group.`,
+    account_user_id: `User ID to login to this account`,
+    website_address: `Website URL / address`,
+    note: `Free form information. See the Sample for some ideas.`
+  }
+}
